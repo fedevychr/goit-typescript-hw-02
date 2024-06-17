7fbe7f18eb4e793729cfbc696b63f43cba669b98
Roman Fedevych <fedevychr@gmail.com>
2024-06-18 00:14:13 +0300

    types LoadMoreBtn

--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -1,6 +1,7 @@
 import css from "./LoadMoreBtn.module.css";
+import { ILoadMoreBtnProps } from "./LoadMoreBtn.types";
 
-const LoadMoreBtn = ({ onLoadMore, children }) => {
+const LoadMoreBtn = ({ onLoadMore, children }: ILoadMoreBtnProps) => {
   return (
     <button className={css.button} onClick={onLoadMore}>
       {children}
---
+++ src/components/LoadMoreBtn/LoadMoreBtn.types.ts
@@ -0,0 +1,6 @@
+import React from "react";
+
+export interface ILoadMoreBtnProps {
+  onLoadMore: () => void;
+  children: React.ReactNode;
+}
